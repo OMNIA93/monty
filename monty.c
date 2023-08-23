4b410850e7fea54deece0d7e830bd048bbdb6900
OMNIA93 <omniannnnnnn@gmail.com>
2023-08-23 09:31:00 -0700

    test

--- monty.c
+++ monty.c
@@ -1,40 +1,19 @@
 #include "monty.h"
-#include <stdio.h>
-#include <stdlib.h>
 
-#define MAX_LINE_LENGTH 256
+execution_env_t execution_env = EXECUTION_ENV_INITIALIZER;
 
+/**
+ * main - Entry point for Monty Interpreter
+ * @argc: number of arguments
+ * @argv: array of character pointers to arguments
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on error
+*/
 int main(int argc, char *argv[])
 {
-    char line[MAX_LINE_LENGTH];
-    FILE *file;
-    stack_t *stack = NULL;
-    unsigned int counter = 0;
+	if (argc != 2)
+	{
+		usage_error();
+	}
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
-
-    file = fopen(argv[1], "r");
-    bus.file = file;
-
-    if (!file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
-
-    while (fgets(line, sizeof(line), file))
-    {
-        counter++;
-        bus.content = line;
-        execute(line, &stack, counter, file);
-    }
-
-    free_stack(stack);
-    fclose(file);
-
-    return 0;
+	return (execute_operations(argv[1]));
 }
