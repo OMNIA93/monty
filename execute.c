f922edd20352091cba17003b2b44c5c7dfbd6be5
OMNIA93 <120645154+OMNIA93@users.noreply.github.com>
2023-08-25 05:58:04 +0300

    Update and rename execute_instructions.c to execute.c

--- execute.c
+++ execute.c
@@ -10,21 +10,21 @@ void (*get_operation(char *opcode))(stack_t **stack, unsigned int line_number)
 {
 	int idx;
 	instruction_t instructions_arr[] = {
-		{"push", push_opcode},
-		{"pall", pall_opcode},
-		{"pint", pint_opcode},
-		{"pop", pop_opcode},
-		{"swap", swap_opcode},
-		{"add", add_opcode},
-		{"nop", nop_opcode},
-		{"sub", sub_opcode},
-		{"div", div_opcode},
-		{"mul", mul_opcode},
-		{"mod", mod_opcode},
-		{"pchar", pchar_opcode},
-		{"pstr", pstr_opcode},
-		{"rotl", rotl_opcode},
-		{"rotr", rotr_opcode},
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{"sub", sub},
+		{"div", div},
+		{"mul", mul},
+		{"mod", mod},
+		{"pchar", pchar},
+		{"pstr", pst},
+		{"rotl", rotl},
+		{"rotr", rotr},
 		{NULL, NULL}
 	};
 
@@ -50,9 +50,9 @@ int execute_operations(char *file_name)
 
 	execution_env.file_pointer = open_file(file_name);
 
-	while ((chars_number = read_line(&buffer_size)) != -1)
+	while ((chars_num = read_line(&buffer_size)) != -1)
 	{
-		line_number++;
+		line_num++;
 		execution_env.line_buffer[chars_number - 1] = '\0';
 		execution_env.tokenized_str = tokenize_string(SPACE);
 		tokenized_str = execution_env.tokenized_str;
@@ -63,9 +63,9 @@ int execute_operations(char *file_name)
 		opcode_func = get_operation(tokenized_str[0]);
 		if (opcode_func == NULL)
 		{
-			invalid_instruction_error(line_number);
+			invalid_instruction_error(line_num);
 		}
-		opcode_func(&execution_env.stack, line_number);
+		opcode_func(&execution_env.stack, line_num);
 
 		free_tokenized_string(execution_env.tokenized_str);
 	}
