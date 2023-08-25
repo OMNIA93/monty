68d67b2be8ddb283b5157d3b3ea496f05d37afc8
OMNIA93 <120645154+OMNIA93@users.noreply.github.com>
2023-08-25 05:52:53 +0300

    Update and rename ascii_operations.c to  pchar_opcode.c

--- pchar_opcode.c
+++ pchar_opcode.c
@@ -9,7 +9,7 @@ void pchar_opcode(stack_t **stack, unsigned int line_number)
 {
 	if (!stack || !*stack)
 	{
-		empty_stack_error(line_number, execution_env.tokenized_str[0]);
+		stack_error(line_number, execution_env.tokenized_str[0]);
 	}
 
 	if (!isascii((*stack)->n))
