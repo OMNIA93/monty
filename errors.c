4a09fbae88e056e67148f8232fd44c0221102d36
OMNIA93 <120645154+OMNIA93@users.noreply.github.com>
2023-08-26 23:07:08 +0300

    Update errors.c

--- errors.c
+++ errors.c
@@ -5,8 +5,8 @@
 */
 void error_1(void)
 {
-	fprintf(stderr, "USAGE: monty file\n");
-	free_all_and_exit();
+fprintf(stderr, "USAGE: monty file\n");
+free_all_and_exit();
 }
 
 /**
@@ -15,8 +15,8 @@ void error_1(void)
 */
 void error_2(char *file_name)
 {
-	fprintf(stderr, "Error: Can't open file %s\n", file_name);
-	free_all_and_exit();
+fprintf(stderr, "Error: Can't open file %s\n", file_name);
+free_all_and_exit();
 }
 
 /**
@@ -24,6 +24,6 @@ void error_2(char *file_name)
 */
 void error_3(void)
 {
-	fprintf(stderr, "Error: malloc failed\n");
-	free_all_and_exit();
+fprintf(stderr, "Error: malloc failed\n");
+free_all_and_exit();
 }
