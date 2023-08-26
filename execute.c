6f0e5113eb3b95c844330d023de091077f782a06
OMNIA93 <120645154+OMNIA93@users.noreply.github.com>
2023-08-26 16:34:59 +0300

    Update execute.c

--- execute.c
+++ execute.c
@@ -60,7 +60,7 @@ int execute_operate(char *file_name)
 		if (check_mode_comment(tokenized_str))
 			continue;
 
-		opcode_func = get_operation(tokenized_str[0]);
+		opcode_func = get_operate(tokenized_str[0]);
 		if (opcode_func == NULL)
 		{
 			invalid_instruction_error(line_num);
