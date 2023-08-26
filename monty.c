503f41aaad5c733e643ce822cad7c304439a0c25
OMNIA93 <120645154+OMNIA93@users.noreply.github.com>
2023-08-26 22:20:51 +0300

    Update monty.c

--- monty.c
+++ monty.c
@@ -10,10 +10,25 @@ execution_env_t execution_env = EXECUTION_ENV_INITIALIZER;
 */
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
-	{
-		error_1();
-	}
+if (argc != 2)
+{
+error_1();
+}
 
-	return (execute_operate(argv[1]));
+return (execute_operate(argv[1]));
+}
+/**
+ * open_file - Opens a file and returns a pointer to FILE structure
+ * @file_name: Name of the file to open
+ * Return: Pointer to the opened file, or NULL on failure
+ */
+FILE *open_file(char *file_name)
+{
+FILE *file = fopen(file_name, "r");
+if (file == NULL)
+{
+perror("Error opening file");
+exit(EXIT_FAILURE);
+}
+return (file);
 }
