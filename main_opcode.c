8b13d3adf3cba5a71fc6606f3b07974465fa416a
OMNIA93 <120645154+OMNIA93@users.noreply.github.com>
2023-08-26 23:35:21 +0300

    Update and rename main_operations.c to main_opcode.c

--- main_opcode.c
+++ main_opcode.c
@@ -3,65 +3,65 @@
 /**
  * push_opcode - Opcode `push` pushes an element to stack in a Monty script
  * @stack: double pointer to the stack structure stack_t
- *         It represents the address of the top element of the stack
+ * It represents the address of the top element of the stack
  * @line_number: line number in the Monty script
  */
 void push_opcode(stack_t **stack, unsigned int line_number)
 {
-	int i;
-	int number;
-	char *opcode = execution_env.tokenized_str[0];
-	char *argument = execution_env.tokenized_str[1];
+int j;
+int number;
+char *opcode = execution_env.tokenized_str[0];
+char *argument = execution_env.tokenized_str[1];
 
-	if (!execution_env.tokenized_str[1])
-		push_non_integer(line_number, opcode);
+if (!execution_env.tokenized_str[1])
+push_non_integer(line_number, opcode);
 
-	for (i = 0; argument[i]; i++)
-	{
-		if (!isdigit(argument[i]) && argument[0] != '-')
-			push_non_integer(line_number, opcode);
-	}
+for (i = 0; argument[j]; j++)
+{
+if (!isdigit(argument[j]) && argument[0] != '-')
+push_non_integer(line_number, opcode);
+}
 
-	number = atoi(argument);
+number = atoi(argument);
 
-	if (execution_env.mode == STACK_MODE)
-		add_node_at_first(stack, number);
-	else if (execution_env.mode == QUEUE_MODE)
-		add_node_at_end(stack, number);
+if (execution_env.mode == STACK_MODE)
+add_node_at_first(stack, number);
+else if (execution_env.mode == QUEUE_MODE)
+add_node_at_end(stack, number);
 }
 
 /**
  * pall_opcode - Opcode `pall` prints all the value of the
- *               stack in a Monty script
+ * stack in a Monty script
  * @stack: head of double pointer to the stack structure stack_t
- *         It represents the address of the top element of the stack
+ * It represents the address of the top element of the stack
  * @line_number: line number in the Monty script
  */
 void pall_opcode(stack_t **stack, unsigned int line_number)
 {
-	UNUSED(line_number);
+UNUSED(line_number);
 
-	if (!stack || !*stack)
-		return;
-
-	print_list(*stack);
+if (!stack || !*stack)
+return;
+	
+print_list(*stack);
 }
 
 /**
  * pint_opcode - Opcode `pint` prints the top of stack in a Monty script
  * @stack: double pointer to the stack structure stack_t
- *         It represents the address of the top element of the stack
+ * It represents the address of the top element of the stack
  * @line_number: line number in the Monty script
  */
 void pint_opcode(stack_t **stack, unsigned int line_number)
 {
-	if (!stack || !*stack)
-	{
-		empty_stack_error(line_number, execution_env.tokenized_str[0]);
-		free_all_and_exit();
-	}
+if (!stack || !*stack)
+{
+empty_stack_error(line_number, execution_env.tokenized_str[0]);
+free_all_and_exit();
+}
 
-	printf("%d\n", (*stack)->n);
+printf("%d\n", (*stack)->n);
 }
 
 /**
@@ -72,23 +72,23 @@ void pint_opcode(stack_t **stack, unsigned int line_number)
  */
 void pop_opcode(stack_t **stack, unsigned int line_number)
 {
-	if (!stack || !*stack)
-	{
-		empty_stack_error(line_number, execution_env.tokenized_str[0]);
-		free_all_and_exit();
-	}
+if (!stack || !*stack)
+{
+empty_stack_error(line_number, execution_env.tokenized_str[0]);
+free_all_and_exit();
+}
 
-	delete_first_node(stack);
+delete_first_node(stack);
 }
 
 /**
  * nop_opcode - Opcode `nop` do nothing
  * @stack: double pointer to the stack structure stack_t
- *         It represents the address of the top element of the stack
+ * It represents the address of the top element of the stack
  * @line_number: line number in the Monty script
  */
 void nop_opcode(stack_t **stack, unsigned int line_number)
 {
-	UNUSED(stack);
-	UNUSED(line_number);
+UNUSED(stack);
+UNUSED(line_number);
 }
