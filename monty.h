67f2359baba28402d37bdf4a9b13166dd2292d74
OMNIA93 <120645154+OMNIA93@users.noreply.github.com>
2023-08-27 01:13:56 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -94,7 +94,7 @@ void error_3(void);
 void invalid_instruction_error(unsigned int line_number);
 void two_elements_error(unsigned int line_number);
 
-void empty_stack_error(unsigned int line_number, char *opcode);
+void pint_error(unsigned int line_number, char *opcode);
 void division_by_zero(unsigned int line_number);
 void ascii_out_of_range(unsigned int line_number, char *opcode);
 void push_non_integer(unsigned int line_number, char *opcode);
