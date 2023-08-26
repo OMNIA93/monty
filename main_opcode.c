b3ae0ffb77c02fec3412c8f74b1d095d1add8b4f
OMNIA93 <120645154+OMNIA93@users.noreply.github.com>
2023-08-27 01:13:51 +0300

    Update main_opcode.c

--- main_opcode.c
+++ main_opcode.c
@@ -58,7 +58,7 @@ void pop_opcode(stack_t **stack, unsigned int line_number)
 {
 if (!stack || !*stack)
 {
-empty_stack_error(line_number, execution_env.tokenized_str[0]);
+pint_error(line_number, execution_env.tokenized_str[0]);
 free_all_and_exit();
 }
 
