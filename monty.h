4976f22057e1f1150d72be82963d6c3f3e077369
OMNIA93 <omniannnnnnn@gmail.com>
2023-08-26 20:41:09 -0700

    test

--- monty.h
+++ monty.h
@@ -2,47 +2,27 @@
 #define MONTY_H
 
 #define _GNU_SOURCE
-
 #include <stdio.h>
-#include <sys/types.h>
-#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 #include <ctype.h>
-
-#define UNUSED(x) (void)(x)
-#define SPACE " "
-#define MAX_TOKENIZED_SIZE 3
-#define COMMENT '#'
-#define EXECUTION_ENV_INITIALIZER { NULL, NULL, NULL, NULL, 0 }
-#define STACK "stack"
-#define QUEUE "queue"
+#include <stdarg.h>
 
 /**
- * enum Mode - defines the mode of operation for linked list
- *
- * @STACK_MODE: the linked list operates as a stack
- * @QUEUE_MODE: the linked list operates as a queue
- */
-enum Mode
-{
-	STACK_MODE = 0,
-	QUEUE_MODE = 1
-};
-
-/**
- * struct stack_s - doubly linked list re-presentation of a stack (or queue)
+ * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack or (queue)
+ * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO
-*/
+ */
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
 } stack_t;
 
 /**
@@ -52,84 +32,53 @@ typedef struct stack_s
  *
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO
-*/
+ */
 typedef struct instruction_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/**
- * struct execution_env - struct to use a group of variables globally
- * @stack: doubly linked list re-presentation of a stack (or queue)
- * @tokenized_str: array of character pointers to the tokenized strings
- * @line_buffer: pointer to buffer that stores opcode
- * @file_pointer: file pointer
- * @mode: stack or queue
-*/
-typedef struct execution_env
-{
-	stack_t *stack;
-	char **tokenized_str, *line_buffer;
-	FILE *file_pointer;
-	int mode;
-} execution_env_t;
+extern stack_t *head;
+typedef void (*op_func)(stack_t **, unsigned int);
 
-extern execution_env_t execution_env;
+/*file operations*/
+void open_file(char *file_name);
+int parse_line(char *buffer, int line_number, int format);
+void read_file(FILE *);
+int len_chars(FILE *);
+void find_func(char *, char *, int, int);
 
-FILE *open_file(char *file_name);
-ssize_t read_line(size_t *buffer_size);
-char **tokenize_string(const char *delim);
+/*Stack operations*/
+stack_t *create_node(int n);
+void free_nodes(void);
+void print_stack(stack_t **, unsigned int);
+void add_to_stack(stack_t **, unsigned int);
+void add_to_queue(stack_t **, unsigned int);
 
-void update_mode(char *opcode);
+void call_fun(op_func, char *, char *, int, int);
 
-void (*get_operate(char *opcode))(stack_t **stack, unsigned int line_number);
-int execute_operate(char *file_name);
-int check_mode(char **tokenized_str);
+void print_top(stack_t **, unsigned int);
+void pop_top(stack_t **, unsigned int);
+void nop(stack_t **, unsigned int);
+void swap_nodes(stack_t **, unsigned int);
 
-void error_1(void);
-void error_2(char *file_name);
-void error_3(void);
+/*Math operations with nodes*/
+void add_nodes(stack_t **, unsigned int);
+void sub_nodes(stack_t **, unsigned int);
+void div_nodes(stack_t **, unsigned int);
+void mul_nodes(stack_t **, unsigned int);
+void mod_nodes(stack_t **, unsigned int);
 
-void invalid_instruction_error(unsigned int line_number);
-void two_elements_error(unsigned int line_number);
+/*String operations*/
+void print_char(stack_t **, unsigned int);
+void print_str(stack_t **, unsigned int);
+void rotl(stack_t **, unsigned int);
 
-void empty_stack_error(unsigned int line_number, char *opcode);
-void division_by_zero(unsigned int line_number);
-void ascii_out_of_range(unsigned int line_number, char *opcode);
-void push_non_integer(unsigned int line_number, char *opcode);
-
-void print_list(const stack_t *head);
-size_t get_list_length(const stack_t *head);
-stack_t *add_node_at_first(stack_t **head, int n);
-stack_t *get_last_node(stack_t **head);
-stack_t *add_node_at_end(stack_t **head, int n);
-stack_t *delete_first_node(stack_t **head);
-
-void push_opcode(stack_t **stack, unsigned int line_number);
-void pall_opcode(stack_t **stack, unsigned int line_number);
-void pint_opcode(stack_t **stack, unsigned int line_number);
-void pop_opcode(stack_t **stack, unsigned int line_number);
-void nop_opcode(stack_t **stack, unsigned int line_number);
-
-
-void add_opcode(stack_t **stack, unsigned int line_number);
-void sub_opcode(stack_t **stack, unsigned int line_number);
-void div_opcode(stack_t **stack, unsigned int line_number);
-void mul_opcode(stack_t **stack, unsigned int line_number);
-void mod_opcode(stack_t **stack, unsigned int line_number);
-
-
-void swap_opcode(stack_t **stack, unsigned int line_number);
-void rotl_opcode(stack_t **stack, unsigned int line_number);
-void rotr_opcode(stack_t **stack, unsigned int line_number);
-
-
-void pchar_opcode(stack_t **stack, unsigned int line_number);
-void pstr_opcode(stack_t **stack, unsigned int line_number);
-
-void free_linked_list(stack_t *head);
-void free_tokenized_string(char **tokenized_str);
-void free_all_and_exit(void);
+/*Error hanlding*/
+void err(int error_code, ...);
+void more_err(int error_code, ...);
+void string_err(int error_code, ...);
+void rotr(stack_t **, unsigned int);
 
 #endif
