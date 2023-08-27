8059a29bac6808d62349453a068def3425c3b337
OMNIA93 <120645154+OMNIA93@users.noreply.github.com>
2023-08-27 19:09:14 +0300

    Update main_opcode.c

--- main_opcode.c
+++ main_opcode.c
@@ -69,13 +69,13 @@ printf("%d\n", (*stack)->n);
  */
 void pop_opcode(stack_t **stack, unsigned int line_number)
 {
-	if (!stack || !*stack)
-	{
-		empty_stack_error(line_number, execution_env.tokenized_str[0]);
-		free_all_and_exit();
-	}
+if (*stack == NULL)
+{
+empty_stack_error(line_number, execution_env.tokenized_str[0]);
+}
 
-	delete_first_node(stack);
+delete_first_node(stack);
+free_all_and_exit();
 }
 
 /**
