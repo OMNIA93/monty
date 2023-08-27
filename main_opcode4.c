271e7773f64f7ad9d1d4cad8150a098a27b649d6
OMNIA93 <120645154+OMNIA93@users.noreply.github.com>
2023-08-28 01:02:29 +0300

    Update main_opcode4.c

--- main_opcode4.c
+++ main_opcode4.c
@@ -8,22 +8,22 @@
 */
 void rotl_opcode(stack_t **stack, unsigned int line_number)
 {
-	stack_t *current_node;
-	int top_value;
+stack_t *current_node;
+int top_value;
 
-	UNUSED(line_number);
+UNUSED(line_number);
 
-	if (!stack || !*stack)
-		return;
+if (*stack == NULL) 
+return;
 
-	top_value = (*stack)->n;
-	current_node = *stack;
-	while (current_node->next)
-	{
-		current_node->n = current_node->next->n;
-		current_node = current_node->next;
-	}
-	current_node->n = top_value;
+top_value = (*stack)->n;
+current_node = *stack;
+while (current_node->next)
+{
+current_node->n = current_node->next->n;
+current_node = current_node->next;
+}
+current_node->n = top_value;
 }
 
 /**
@@ -33,22 +33,22 @@ void rotl_opcode(stack_t **stack, unsigned int line_number)
 */
 void rotr_opcode(stack_t **stack, unsigned int line_number)
 {
-	stack_t *current_node;
-	int prev_node_value, tmp;
+stack_t *current_node;
+int prev_node_value, j;
 
-	UNUSED(line_number);
+UNUSED(line_number);
 
-	if (!stack || !*stack)
-		return;
+if (*stack == NULL)
+return;
 
-	tmp = (*stack)->n;
-	current_node = (*stack)->next;
-	while (current_node)
-	{
-		prev_node_value = tmp;
-		tmp = current_node->n;
-		current_node->n = prev_node_value;
-		current_node = current_node->next;
-	}
-	(*stack)->n = tmp;
+j = (*stack)->n;
+current_node = (*stack)->next;
+while (current_node)
+{
+prev_node_value = j;
+j = current_node->n;
+current_node->n = prev_node_value;
+current_node = current_node->next;
+}
+(*stack)->n = j;
 }
