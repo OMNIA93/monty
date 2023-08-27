ddca67d8d61eaf250f78265f39d7523103d31aef
OMNIA93 <120645154+OMNIA93@users.noreply.github.com>
2023-08-27 16:38:48 +0300

    Update main_opocode2.c

--- main_opocode2.c
+++ main_opocode2.c
@@ -8,12 +8,11 @@
 void add_opcode(stack_t **stack, unsigned int line_number)
 {
 int value1, value2, result;
-    
+
 if (get_list_length(*stack) < 2)
 {
 two_elements_error(line_number);
-}
-    
+}    
 value1 = (*stack)->n;
 delete_first_node(stack);
 value2 = (*stack)->n;
