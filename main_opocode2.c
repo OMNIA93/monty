51322ad248221a2d80d5a4bc1839f0344f85bd55
OMNIA93 <120645154+OMNIA93@users.noreply.github.com>
2023-08-27 16:40:48 +0300

    Update main_opocode2.c

--- main_opocode2.c
+++ main_opocode2.c
@@ -12,12 +12,11 @@ int value1, value2, result;
 if (get_list_length(*stack) < 2)
 {
 two_elements_error(line_number);
-}    
+}
 value1 = (*stack)->n;
 delete_first_node(stack);
 value2 = (*stack)->n;
 delete_first_node(stack);
-    
 result = value1 + value2;
 add_node_at_first(stack, result);
 }
