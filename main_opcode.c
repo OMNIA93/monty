469dc228e0d33f97f5f117ec8da2d107c654690e
OMNIA93 <120645154+OMNIA93@users.noreply.github.com>
2023-08-27 18:55:44 +0300

    Update main_opcode.c

--- main_opcode.c
+++ main_opcode.c
@@ -53,13 +53,15 @@ print_list(*stack);
  */
 void pint_opcode(stack_t **stack, unsigned int line_number)
 {
-if (!stack || !*stack)
-{
-empty_stack_error(line_number, execution_env.tokenized_str[0]);
-free_all_and_exit();
+	if (!stack || !*stack)
+	{
+		empty_stack_error(line_number, execution_env.tokenized_str[0]);
+		free_all_and_exit();
+	}
+
+	delete_first_node(stack);
 }
-delete_first_node(stack);
-}
+
 /**
  * pop_opcode - Opcode `pop` removes the top of stack in a Monty script
  * @stack: double pointer to the stack structure stack_t
