ebd4122dbe82cf380eb3260e32625a2eddc0b047
OMNIA93 <120645154+OMNIA93@users.noreply.github.com>
2023-08-28 05:43:53 +0300

    Create linked_list2.c

---
+++ linked_list2.c
@@ -0,0 +1,44 @@
+#include "monty.h"
+
+/**
+ * print_list - prints all the elements of a list
+ * @first: first of the list
+ * Return: the number of nodes
+ */
+void print_list(const stack_t *first)
+{
+	const stack_t *current;
+
+	if (!first)
+		return;
+
+	current = first;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
+
+/**
+ * delete_first_node - delete the first node
+ * @first: pointer to the first of stack
+ * Return: address of new head or NULL if empty
+ */
+stack_t *delete_first_node(stack_t **first)
+{
+	stack_t *temp = *first;
+
+	if (*first == NULL)
+		return (NULL);
+
+	*first = (*first)->next;
+	if (*head != NULL)
+		(*head)->prev = NULL;
+
+	first->next = NULL;
+	free(first);
+
+	return (*first);
+}
