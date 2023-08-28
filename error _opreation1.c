625c182baf442ac2619f934c27e08accf8c7cde2
OMNIA93 <120645154+OMNIA93@users.noreply.github.com>
2023-08-28 04:22:31 +0300

    Update error _opreation1.c

--- error _opreation1.c
+++ error _opreation1.c
@@ -1,17 +1,6 @@
 #include "monty.h"
 
 /**
- * invalid_error - prints to stderr when the opcode is inavlid
- * @line_number: line number where the instruction appear in the file
-*/
-void invalid_error(unsigned int line_number)
-{
-	fprintf(stderr, "L%d: unknown instruction %s\n", line_number,
-					execution_env.tokenized_str[0]);
-	free_and_exit();
-}
-
-/**
  * swap_error - prints to stderr when stack has less than 2 elements
  * @line_number: line number where the instruction appear in the file
 */
@@ -28,3 +17,15 @@ void swap_error(unsigned int line_number)
 					execution_env.tokenized_str[0], mode);
 	free_and_exit();
 }
+
+/**
+ * invalid_error - prints to stderr when the opcode is inavlid
+ * @line_number: line number where the instruction appear in the file
+*/
+void invalid_error(unsigned int line_number)
+{
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_number,
+					execution_env.tokenized_str[0]);
+	free_and_exit();
+}
+
